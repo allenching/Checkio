64cdcafe260114d54a1861ebd8545cb5cb2f7b51
allenching <34501113+allenching@users.noreply.github.com>
2017-12-13 15:02:40 +0800

    Add files via upload

---
+++ Hamming_Distance.py
@@ -0,0 +1,29 @@
+"""
+Description:
+The Hamming distance between two binary integers is the number of bit positions that differs (read more about the Hamming distance on Wikipedia). For example:
+
+    117 = 0 1 1 1 0 1 0 1
+     17 = 0 0 0 1 0 0 0 1
+      H = 0+1+1+0+0+1+0+0 = 3
+
+You are given two positive numbers (N, M) in decimal form. You should calculate the Hamming distance between these two numbers in binary form.
+
+Input: Two arguments as integers.
+
+Output: The Hamming distance as an integer.
+"""
+
+import operator
+def checkio(n, m):
+    mxorn = operator.xor(n, m)
+    binmxorn = "{0:b}".format(mxorn)
+    count = 0
+    for i in binmxorn:
+        count += int(i)
+    return count
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(117, 17) == 3, "First example"
+    assert checkio(1, 2) == 2, "Second example"
+    assert checkio(16, 15) == 5, "Third example"
