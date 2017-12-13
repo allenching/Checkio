db42821b43b127a2b8c7001988321d176cbf010b
allenching <34501113+allenching@users.noreply.github.com>
2017-12-13 14:45:15 +0800

    Add files via upload

---
+++ Non-unique_Elements.py
@@ -0,0 +1,26 @@
+"""
+Description:
+u are given a non-empty list of integers (X). For this task, you should return a list consisting of only the non-unique elements in this list. To do so you will need to remove all unique elements (elements which are contained in a given list only once). When solving this task, do not change the order of the list. Example: [1, 2, 3, 1, 3] 1 and 3 non-unique elements and result will be [1, 3, 1, 3].
+
+Input: A list of integers.
+
+Output: The list of integers.
+"""
+def checkio(data):
+    finallist = []*len(data)
+    for i in data:
+        count = 0
+        for n in range(len(data)):
+            if i == data[n]:
+                count += 1
+        if count >= 2:
+            finallist.append(i)        
+    data = finallist
+    return data
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert list(checkio([1, 2, 3, 1, 3])) == [1, 3, 1, 3], "1st example"
+    assert list(checkio([1, 2, 3, 4, 5])) == [], "2nd example"
+    assert list(checkio([5, 5, 5, 5, 5])) == [5, 5, 5, 5, 5], "3rd example"
+    assert list(checkio([10, 9, 10, 10, 9, 8])) == [10, 9, 10, 10, 9], "4th example"
